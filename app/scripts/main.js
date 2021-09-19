a3ae18c9a2aa15b6b741179983c48e8ac5ce3ba8
nguyenbinhminh <romeominh@gmail.com>
2021-09-19 17:09:27 +0700

    update package hongkong and some bug

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,10 +10,28 @@ $(document).ready(function () {
 $('[data-toggle="datepicker"]').datepicker({
 	format: "mm-dd-yyyy",
 });
+// Accordion
+$(document).ready(function () {
+	var acc = document.getElementsByClassName("package-content-program-header");
+	var i;
+	for (i = 0; i < acc.length; i++) {
+		acc[i].addEventListener("click", function () {
+			this.classList.toggle("active");
+			var panelAcc = this.nextElementSibling;
+			if (panelAcc.style.display === "block") {
+				panelAcc.style.display = "none";
+			} else {
+				panelAcc.style.display = "block";
+			}
+		});
+	}
+});
 // Price bar
-var slider = document.getElementById("filter-price-progress");
-var output = document.getElementById("filter-label--value");
-output.innerHTML = slider.value;
-slider.oninput = function () {
-	output.innerHTML = this.value;
-};
+$(document).ready(function () {
+	var slider = document.getElementById("filter-price-progress");
+	var output = document.getElementById("filter-label--value");
+	output.innerHTML = slider.value;
+	slider.oninput = function () {
+		output.innerHTML = this.value;
+	};
+});
